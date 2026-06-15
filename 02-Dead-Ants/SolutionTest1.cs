46e92e63b03f8701c484e6ab62dc90c57e99cec6
mmmaaaaaa <411035006@gms.ndhu.edu.tw>
2026-06-15 23:21:55 +0800

    add 02_Dead_Ants

---
+++ 02-Dead-Ants/SolutionTest1.cs
@@ -0,0 +1,39 @@
+﻿namespace _02_Dead_Ants;
+
+public class DeadAntsTest
+{
+    [Fact]
+    public void GivenValidInput_ReturnDeadAntsCount()
+    {
+        var result = Solution.CountDeadAnts("...ant...ant..nat.ant.t..ant...ant..ant..ant.anant..t");
+        Assert.Equal(3, result);
+    }
+
+    [Fact]
+    public void GivenEmptyString_ReturnZero()
+    {
+        var result = Solution.CountDeadAnts("");
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void GivenOneAnt_ReturnZero()
+    {
+        var result = Solution.CountDeadAnts("ant");
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void GivenWholeAnt_ReturnZero()
+    {
+        var result = Solution.CountDeadAnts("..ant..ant.ant...ant.ant..ant.ant....ant..ant.ant.ant...ant..");
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void GivenALLDiedAnt_ReturnDeadAntsCount()
+    {
+        var result = Solution.CountDeadAnts("a..n.t..");
+        Assert.Equal(1, result);
+    }
+}
