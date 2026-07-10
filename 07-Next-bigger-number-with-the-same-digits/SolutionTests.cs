7342d196a7fd5874322a0ab3826f28e4b6574322
mmmaaaaaa <411035006@gms.ndhu.edu.tw>
2026-07-10 17:07:33 +0800

    add 07 Next bigger number with the same digits

---
+++ 07-Next-bigger-number-with-the-same-digits/SolutionTests.cs
@@ -0,0 +1,18 @@
+﻿namespace _07_Next_bigger_number_with_the_same_digits;
+
+public class NextBiggerNumberTests
+{
+    [Fact]
+    public void GivenNormalPermutation_ReturnCorrectResult()
+    {
+        var result = Solution.NextBiggerNumber(2017);
+        Assert.Equal(2071, result);
+    }
+
+    [Fact]
+    public void GivenNoPermutation_ReturnMinusOne()
+    {
+        var result = Solution.NextBiggerNumber(111);
+        Assert.Equal(-1, result);
+    }
+}
