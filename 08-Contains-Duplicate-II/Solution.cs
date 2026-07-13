c483b4ff796803b7467ddad77839699416d4c787
mmmaaaaaa <411035006@gms.ndhu.edu.tw>
2026-07-13 18:25:39 +0800

    refactor 08 Contains Duplicate II

--- 08-Contains-Duplicate-II/Solution.cs
+++ 08-Contains-Duplicate-II/Solution.cs
@@ -7,18 +7,11 @@ public class Solution
         var dict = new Dictionary<int, int>();
         for (int i = 0; i < nums.Length; i++)
         {
-            if (dict.ContainsKey(nums[i]))
+            if (dict.ContainsKey(nums[i]) && i - dict[nums[i]] <= k)
             {
-                if (i - dict[nums[i]] <= k)
-                {
-                    return true;
-                }
-                dict[nums[i]] = i;
+                return true;
             }
-            else
-            {
-                dict.Add(nums[i], i);
-            }
+            dict[nums[i]] = i;
         }
         return false;
     }
