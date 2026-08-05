fb0475b1965929f2959b97abbd6fe37b2ea5e7c3
mmmaaaaaa <411035006@gms.ndhu.edu.tw>
2026-08-05 13:06:39 +0800

    refactor 36 Find The Parity Outlier

--- 36-Find-The-Parity-Outlier/Solution.cs
+++ 36-Find-The-Parity-Outlier/Solution.cs
@@ -4,19 +4,8 @@ public class Solution
 {
     public static int Find(int[] integers)
     {
-        var odd_count = 0;
-        var even_count = 0;
-        for (int i = 0; i < integers.Length; i++)
-        {
-            if (integers[i] % 2 == 0)
-            {
-                even_count++;
-            }
-            else
-            {
-                odd_count++;
-            }
-        }
+        var even_count = integers.Count(x => x % 2 == 0);
+        var odd_count = integers.Count(x => x % 2 != 0);
         if (odd_count < even_count)
         {
             return integers.First(x => x % 2 != 0);
